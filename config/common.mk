8b51c8990f20ff9bde70d6b4924c866cf1380a0b
Rashed Abdel-Tawab <rashed@linux.com>
2018-03-02 10:21:37 -0500

    DU: Move to Google's method of defining system default props
    PRODUCT_SYSTEM_DEFAULT_PROPERTIES can now be used to define system default
    properties which should be installed in system partition.
    
    Those props are stored in /system/etc/prop.default.
    
    Change-Id: I6cb0e28a7599b010b389cc541015a37010a00f4b

--- config/common.mk
+++ config/common.mk
@@ -1,15 +1,15 @@
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
 ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
-PRODUCT_PROPERTY_OVERRIDES += \
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.com.google.clientidbase=android-google
 else
-PRODUCT_PROPERTY_OVERRIDES += \
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.com.google.clientidbase=$(PRODUCT_GMS_CLIENTID_BASE)
 endif
 
 # General additions
-PRODUCT_PROPERTY_OVERRIDES += \
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     keyguard.no_require_sim=true \
     dalvik.vm.debug.alloc=0 \
     ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
